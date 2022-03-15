c9165b3c49c0d76f6c0cdcdab7e16f4d035345de
guilhermebrejeiro <juanbre@gmail.com>
2022-03-15 10:56:07 -0300

    adding __init__ just on functions folder

--- modules/main.py
+++ modules/main.py
@@ -7,8 +7,9 @@ from modules.ml_support_modules.functions.feat_eng import process_data
 app = FastAPI()
 
 
+
 # Open the config.json file get the paths variables
-with open('../config.json', 'r') as conf:
+with open('./config.json', 'r') as conf:
     config = json.load(conf)
 # Load model
 model_path = os.path.join(config['production_model'], "model_wine_quality.joblib")
